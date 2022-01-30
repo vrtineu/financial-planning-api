c90978246535dcb61464a060d150fc721dbc1695
vrtineu <vininro@gmail.com>
2022-01-29 22:53:06 -0300

    add tests using jest

--- src/__tests__/utils/index.ts
+++ src/__tests__/utils/index.ts
@@ -1,6 +1,7 @@
 import app from "../..";
 import Counters from "../../models/Counters";
 import { defaultMessages } from "../../utils/responseStatusCode";
+import Despesas from "../../models/Despesas";
 import Receitas from "../../models/Receitas";
 import request from "supertest";
 
@@ -10,9 +11,21 @@ const dadosReceita = {
   data: new Date(),
 };
 
+const dadosDespesa = {
+  descricao: "Test",
+  valor: 1000,
+  data: new Date(),
+  categoria: "Moradia",
+};
+
 async function limparReceitas() {
   await Receitas.deleteMany({});
-  await Counters.deleteMany({});
+  await Counters.findOneAndDelete({ id: "idReceita" });
+}
+
+async function limparDespesas() {
+  await Despesas.deleteMany({});
+  await Counters.findOneAndDelete({ id: "idDespesa" });
 }
 
 async function criarReceita(dadosReceita: object) {
@@ -27,4 +40,23 @@ async function criarReceita(dadosReceita: object) {
   return receita;
 }
 
-export { dadosReceita, limparReceitas, criarReceita };
+async function criarDespesa(dadosDespesa: object) {
+  const despesa = await request(app)
+    .post("/api/despesas")
+    .send(dadosDespesa)
+    .then((res) => {
+      expect(res.status).toBe(201);
+      expect(res.body).toBe(defaultMessages.success);
+    });
+
+  return despesa;
+}
+
+export {
+  dadosReceita,
+  dadosDespesa,
+  limparReceitas,
+  limparDespesas,
+  criarReceita,
+  criarDespesa,
+};
